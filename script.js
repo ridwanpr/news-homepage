3ada1542d972a81a425a7afa6a5a04afb724b10b
Ridwan <ridwanpr@outlook.com>
2025-12-27 04:53:18 +0700

    style mobile hamburger menu

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+const hamburgerIcon = document.querySelector(".hamburger-icon");
 const featuredImg = document.querySelector(".featured__img");
+const overlay = document.getElementById("overlay");
+const nav = document.getElementById("nav");
+const closeMenu = document.querySelector(".close");
 
-function updateImage() {
+function openHamburgerMenu() {
+  overlay.classList.remove("none");
+  nav.classList.add("active");
+}
+
+function closeHamburgerMenu() {
+  overlay.classList.add("none");
+  nav.classList.remove("active");
+}
+
+function updateFeaturedImage() {
   const windowWidth = window.innerWidth;
   if (windowWidth >= 1024) {
     featuredImg.setAttribute("src", "assets/images/image-web-3-desktop.jpg");
   } else {
     featuredImg.setAttribute("src", "assets/images/image-web-3-mobile.jpg");
   }
+  closeHamburgerMenu();
 }
 
-window.addEventListener("resize", updateImage);
+window.addEventListener("resize", updateFeaturedImage);
+hamburgerIcon.addEventListener("click", openHamburgerMenu);
+closeMenu.addEventListener("click", closeHamburgerMenu);
 updateImage();
